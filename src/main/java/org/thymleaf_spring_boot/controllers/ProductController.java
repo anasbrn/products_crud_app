e0dca4cae34a28f78f54180ec450b90642f3320b
BARNOCH Anas <anasbarnoch@gmail.com>
2025-10-22 21:41:39 +0100

    Fix unhealthy prb

--- src/main/java/org/thymleaf_spring_boot/controllers/ProductController.java
+++ src/main/java/org/thymleaf_spring_boot/controllers/ProductController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/products")
 public class ProductController {
     private ProductRepository productRepository;
 
@@ -18,26 +17,31 @@ public class ProductController {
     }
 
     @GetMapping("")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("products")
     public String listProducts(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("products", products);
         return "products_view";
     }
 
-    @PostMapping("/delete/{id}")
+    @PostMapping("/products/delete/{id}")
     public String deleteProduct(@PathVariable(name = "id") Long id) {
         productRepository.deleteById(id);
         return "redirect:/products";
     }
 
-    @PostMapping("/store")
+    @PostMapping("/products/store")
     public String storeProduct(@RequestParam(name = "name") String name, @RequestParam(name = "price") Double price, @RequestParam(name = "description") String description) {
         Product product = Product.builder().name(name).price(price).description(description).build();
         productRepository.save(product);
         return "redirect:/products";
     }
 
-    @PostMapping("/update/{id}")
+    @PostMapping("/products/update/{id}")
     public String updateProduct(@PathVariable(name = "id") Long id, @RequestParam(name = "name") String name, @RequestParam(name = "price") Double price, @RequestParam(name = "description") String description) {
         Product product = productRepository.findById(id).get();
         product.setName(name);
@@ -47,19 +51,19 @@ public class ProductController {
         return "redirect:/products";
     }
 
-    @GetMapping("/all")
+    @GetMapping("/products/all")
     @ResponseBody
     public List<Product> all() {
         return productRepository.findAll();
     }
 
-    @GetMapping("/api/store")
+    @GetMapping("/products/api/store")
     @ResponseBody
     public Product storeApi() {
         Product product = Product.builder().name("name").price(200.0).description("description").build();
         return productRepository.save(product);
     }
-    @GetMapping("/api/delete/{id}")
+    @GetMapping("/products/api/delete/{id}")
     @ResponseBody
     public void deleteApi(@PathVariable(name = "id") Long id) {
         productRepository.deleteById(id);
